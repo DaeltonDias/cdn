f4fe909f3a47bb1eff593270d1f9a0066a5faafa
Daelton Dias <daelton.dias@gmail.com>
2022-09-23 03:09:44 -0300

    Update

---
+++ www/file-to-base64.js
@@ -0,0 +1,10 @@
+const FileToBase64 = {
+  convert: (file, success, error) => new Promise((resolve, reject) => {
+    if (!file) (error || reject)('The argument passed is not a file!'); else {
+      const reader = new FileReader(); reader.readAsDataURL(file);
+      reader.onload = () => (success || resolve)(reader.result);
+      reader.onerror = err => (error || reject)(err);
+    }
+  })
+};
+
